2604ab5eeb85fdc4136b04ab02e9d491a54fd826
zouyinping <916103108@qq.com>
2020-04-21 23:06:58 +0800

    add file

---
+++ pages/enent/detail.js
@@ -0,0 +1,109 @@
+// pages/enent/detail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    active: 0,
+    message: '测试',
+    fileList: [],
+    stepActive: 1,
+    steps: [
+      {
+        text: '步骤一',
+        desc: '描述信息'
+      },
+      {
+        text: '步骤二',
+        desc: '描述信息'
+      },
+      {
+        text: '步骤三',
+        desc: '描述信息'
+      },
+      {
+        text: '步骤四',
+        desc: '描述信息'
+      }
+    ],
+    radio: '1'
+  },
+  onChange(){},
+  onRadioChange(event) {
+    this.setData({
+      radio: event.detail
+    });
+  },
+  afterRead(event) {
+    const { file } = event.detail;
+    // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
+    wx.uploadFile({
+      url: 'https://example.weixin.qq.com/upload', // 仅为示例，非真实的接口地址
+      filePath: file.path,
+      name: 'file',
+      formData: { user: 'test' },
+      success(res) {
+        // 上传完成需要更新 fileList
+        const { fileList = [] } = this.data;
+        fileList.push({ ...file, url: res.data });
+        this.setData({ fileList });
+      }
+    });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
