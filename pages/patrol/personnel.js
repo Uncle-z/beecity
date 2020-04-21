c512e64fcfba7679324953f62fd96f5b79bbd2ef
zouyinping <916103108@qq.com>
2020-04-22 00:13:44 +0800

    add file

---
+++ pages/patrol/personnel.js
@@ -0,0 +1,91 @@
+// pages/patrol/personnel.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    option1: [
+      { text: '楼栋1', value: 0 },
+      { text: '楼栋2', value: 1 },
+      { text: '楼栋3', value: 2 }
+    ],
+    option2: [
+      { text: '单元1', value: 'a' },
+      { text: '单元2', value: 'b' },
+      { text: '单元3', value: 'c' }
+    ],
+    option3: [
+      { text: '楼层1', value: 'a' },
+      { text: '楼层2', value: 'b' },
+      { text: '楼层3', value: 'c' }
+    ],
+    option4: [
+      { text: '业主', value: 'a' },
+      { text: '租户', value: 'b' },
+      { text: '亲戚', value: 'c' }
+    ],
+    value1: 0,
+    value2: 'a',
+    value3: 'a',
+    value4: 'a'
+  },
+  scroll(e) {
+
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
